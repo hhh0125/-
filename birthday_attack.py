64a3fdd7c776e4df39d90a8b3f55ad922d71f3d8
hhh0125 <139990267+hhh0125@users.noreply.github.com>
2023-07-19 23:04:46 +0800

    Add files via upload

---
+++ birthday_attack.py
@@ -0,0 +1,33 @@
+import sm3
+
+def birth_attack(l):
+    # 需要穷举的原像空间
+    num=int(2**(l/2))
+    # 以hash值为索引
+    hash={}
+    for i in range(num):
+        # hash前l比特
+        i_hash = sm3.sm3_hash(i)[2:l]
+        index=int(i_hash,2)
+        if index not in hash:
+            hash[index]=i
+        else:
+            return i_hash,i,hash[index]
+    return None
+
+
+if __name__=='__main__':
+
+
+
+    l=48
+    print(f'寻找碰撞的空间大小：{l}bit')
+    result=birth_attack(l)
+    if result is not None:
+        hash,x,y=result
+        print(f"找到一组碰撞{x}，{y};hash值为{hash}")
+    else:
+        print('未发现碰撞')
+
+
+
